77e8bbfc76ab020d323803b30c182aadca91bf27
Valerii Ruchko <valerii.ruchko@gmail.com>
2022-12-20 18:50:33 +0100

    Added get and post requests to /databases route

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 
 //our imported files:
 const db = require('./queries');
+// dictionary to store all of our pools //// key is name of db, as a string
+const pools = new Map();
+// creating new pool to connect to our starting database:
+const mainPool = db.createPool("it_dbms_lab");
+// adding new pool to our map
+pools.set("it_dbms_lab", mainPool);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -15,12 +21,76 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 // our REST-API
-app.get("/", (req, res) => {        
-            console.log("GET-request on root route");   //sends a response
-            res.json({info: "Node.js, Postgress and myass"});
+app.get("/", (request, response) => {
+            //sends a response
+            response.json({info: "It's starting page of our app"});
         });
 
-app.get("/all-dbs", db.getUsers);
+// collection of HTTP-requests for our /databases route /// we implement getting databases list and creating new database
+app.route("/databases")
+    .get( (request, response) => {
+        pools.get("it_dbms_lab").query('SELECT * FROM virtual_databases', (error, results) => {
+            if (error) {
+                throw error;
+            }
+            response.status(200).json(results.rows);
+        })
+    })
+    .post ( (request, response) => {
+        // getting our db name from form-data
+        const user_db_name = request.body.db_name;
+        let db_exists = true;
+
+        pools.get("it_dbms_lab").connect((err, client, release) => {
+            if (err) {
+                return console.error('Error acquiring clien', err.stack)
+            }; 
+            client.query('SELECT datname FROM pg_database WHERE datname = ($1)', [user_db_name] ,(error, results) => {
+                if (error) {
+                    throw error;
+                }
+                if (results.rows[0] === undefined) {
+                    db_exists = false;
+                }
+                //console.log(results.rows[0]);
+                console.log(results.rows[0] === undefined);
+                release();
+           })  
+        })
+        
+        
+        if(!db_exists === false) {
+            // creating new database if it doesn't exist
+            pools.get("it_dbms_lab").connect((err, client, release) => {
+                if (err) {
+                    return console.error('Error acquiring clien', err.stack)
+                }; 
+                client.query(`CREATE DATABASE ${user_db_name}`, (error, results) => {
+                    if (error) {
+                        console.log(error.stack);
+                        response.status(400).send('DB already exists');
+                    } else {
+                        console.log(`Database ${user_db_name} was created successfully`);
+                        response.status(200).json({db_name: user_db_name});
+                    }
+                })
+                // inserting db name to our main db
+                client.query('INSERT INTO virtual_databases (db_name) VALUES ($1) RETURNING *', [user_db_name], (error, results) => {
+                    if (error) {
+                        console.log(error.stack.error);
+                    }
+                })
+                release();
+            })
+        }
+        pools.set(user_db_name, db.createPool("user_db_name"));
+        console.log(pools);
+    });
+
+// app.route("/databases/:id")
+//     .get(db.getTables)
+//     .post(db.createTable)
+//     .delete(db.deleteTable);
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
--- server/queries.js
+++ server/queries.js
@@ -1,22 +1,74 @@
 const Pool = require('pg').Pool;
+const Client = require('pg').Client;
 
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'it_dbms_lab',
-    password: 'g2923133g',
-    port: 5432,
-})
-// our queries for database:
-const getUsers = (request, response) => {
-    pool.query('SELECT * FROM virtual_databases', (error, results) => {
-        if (error) {
-            throw error;
-        }
-        response.status(200).json(results.rows);
+const createPool = (database_name) => {
+    // creating new pool to connect to our db
+    const pool = new Pool({
+        user: 'postgres',
+        host: 'localhost',
+        database: `${database_name}`,
+        password: 'g2923133g',
+        port: 5432,
     })
-}
-//exporting all queries to index.js
+
+    return pool;
+};
+
+// const createClient = (database_name) => {
+//     // creating new client to connect to our db
+//     const client = new Client({
+//         user: 'postgres',
+//         host: 'localhost',
+//         database: `${database_name}`,
+//         password: 'g2923133g',
+//         port: 5432,
+//     })
+
+//     return client;
+// };
+
+// --------------------------------------------------------------------------------------------------------------------------------
+
+// our queries for route "/databases/:id"
+// const getTables = (request, response) => {
+//     const db_identifier = parseInt(request.params.id); 
+
+//     pool.query('SELECT * FROM virtual_tables_list WHERE belongs_to_db = $1', [db_identifier], (error, results) => {
+//         if (error) {
+//             throw error;
+//         }
+//         response.status(200).json(results.rows[0].table_name);
+//     })
+// }
+
+// const createTable = (request, response) => {
+//     const req_data = request.body;
+//     const db_identifier = parseInt(request.params.id);
+
+//     pool.query('INSERT INTO virtual_tables_list (belongs_to_db, table_name) VALUES ($1, $2) RETURNING *', [db_identifier, req_data.table_name], (error, results) => {
+//         if (error) {
+//             throw error;
+//         }
+//         response.status(200).send(`Table with name ${results.rows[0].table_name} is added`);
+//     })
+// }
+
+// const deleteTable = (request, response) => {
+//     // getting our db name from form-data
+//     const req_data = request.body;
+
+//     pool.query('INSERT INTO virtual_databases (db_name) VALUES ($1) RETURNING *', [req_data.db_name], (error, results) => {
+//         if (error) {
+//             throw error;
+//         }
+//         response.status(200).send(`Database with name ${results.rows[0].db_name} is added`);
+//     })
+// }
+
+// --------------------------------------------------------------------------------------------------------------------------------
+
+//exporting our dynamic poolCreator to index.js
 module.exports = {
-    getUsers
+    createPool
+    // createClient
 }
